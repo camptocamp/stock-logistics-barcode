2572a35dfad4905eb3a4440f3605bdaab2ff7793
Angel Moya - PESOL <amoyapardo@gmail.com>
2017-06-06 15:54:35 +0200

    FIX js lint

--- stock_scanner_web/static/src/js/stock_scanner_web.tour.js
+++ stock_scanner_web/static/src/js/stock_scanner_web.tour.js
@@ -16,72 +16,72 @@ odoo.define("stock_scanner_web.tour", function (require) {
           {
               title: "Click Tutorial",
               element: "a:contains('Tutorial')",
-              waitFor: "a:contains('Tutorial')",
+              waitFor: "a:contains('Tutorial')"
           },
           {
               title: "Click Step types",
               element: "a:contains('Step types')",
-              waitFor: "a:contains('Step types')",
+              waitFor: "a:contains('Step types')"
           },
           {
               title: "Click Go",
               element: "#btn-go",
-              waitFor: "#btn-go",
+              waitFor: "#btn-go"
           },
           {
               title: "Click Go",
               element: "#btn-go",
-              waitFor: "#btn-go",
+              waitFor: "#btn-go"
           },
           {
               title: "Click Go to Error step",
               element: ".js_item:contains('Go to Error step')",
-              waitFor: ".js_item:contains('Go to Error step')",
+              waitFor: ".js_item:contains('Go to Error step')"
           },
           {
               title: "Click Back",
               element: "#btn-back",
-              waitFor: "#btn-back",
+              waitFor: "#btn-back"
           },
           {
               title: "Click Go to next step",
               element: "a:contains('Go to next step')",
-              waitFor: "a:contains('Go to next step')",
+              waitFor: "a:contains('Go to next step')"
           },
           {
               title: "Click Yes",
               element: "#btn-yes",
-              waitFor: "#btn-yes",
+              waitFor: "#btn-yes"
           },
           {
               title: "Click Submit",
               element: ".btn-primary",
-              waitFor: ".btn-primary",
+              waitFor: ".btn-primary"
           },
           {
               title: "Click Submit",
               element: ".btn-primary",
-              waitFor: ".btn-primary",
+              waitFor: ".btn-primary"
           },
           {
               title: "Click Submit",
               element: ".btn-primary",
-              waitFor: ".btn-primary",
+              waitFor: ".btn-primary"
           },
           {
               title: "Click Back",
               element: "#btn-back",
-              waitFor: "#btn-back",
+              waitFor: "#btn-back"
           },
           {
               title: "Click Tutorial",
               element: "a:contains('Tutorial')",
-              waitFor: "a:contains('Tutorial')",
+              waitFor: "a:contains('Tutorial')"
           },
           {
               title: "Stock Scanner Web Type",
               element: "a:contains('Stock Scanner Web Type')",
-              waitFor: "a:contains('Stock Scanner Web Type')",
+              waitFor: "a:contains('Stock Scanner Web Type')"
           },
         ]
     });
